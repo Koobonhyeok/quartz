52f04cb2ae57e8ee8bc542db79d737f2c1a38214
koobonhyeok <korea6441@gmail.com>
2023-03-22 23:33:07 +0900

    commit

---
+++ src/main/java/com/study/quartz/service/QuartzJob.java
@@ -0,0 +1,23 @@
+package com.study.quartz.service;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+public class QuartzJob extends QuartzJobBean {
+
+	private QuartzService service;
+	
+	@Override
+	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+		try {
+			service = ((ApplicationContext) context.getScheduler().getContext().get("applicationContext")).getBean(QuartzService.class);
+			service.quartzService();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+		
+	}
+
+}
---
+++ src/main/java/com/study/quartz/service/QuartzServiceImpl.java
@@ -0,0 +1,18 @@
+package com.study.quartz.service;
+
+import org.springframework.stereotype.Service;
+
+@Service("quartzService")
+public class QuartzServiceImpl implements QuartzService {
+
+	@Override
+	public void quartzService() {
+		try {
+			
+			System.out.println( "Quartz Cluster");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
